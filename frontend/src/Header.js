5b289755bcd28e3f3c4c2251445ebacc519122cf
sahilverma2k <61312202+sahilverma2k@users.noreply.github.com>
2020-09-27 00:03:06 +0530

    Add files via upload

---
+++ frontend/src/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <p>AVIATO</p>
+        <img src="https://pngimg.com/uploads/jet_fighter/jet_fighter_PNG34.png" />
+      </div>
+      <div className="header__navbar">
+        <ul>
+          <li>
+            <span className="explore">EXPLORE</span>
+          </li>
+          <li>
+            <span className="shop">SHOP</span>
+          </li>
+          <li>
+            <span className="login">LOGIN</span>
+          </li>
+          <li>
+              <SearchIcon />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
