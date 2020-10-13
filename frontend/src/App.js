08139e496b8e3bd3884867fee3eb855109ca3979
Sammy Nyakabau <nyakabausammy8@gmail.com>
2020-10-13 13:58:14 +0530

    Add react router 🚀

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 /* eslint-disable */
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Footer from "./Footer";
 
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-
-      
+      <Switch>
+        <Route path="/">
+          <Header />
+          <Home />
+          <Footer />
+        </Route>
+      </Switch>
     </div>
   );
 }
