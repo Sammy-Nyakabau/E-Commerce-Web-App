aaa08dd8fe63549650236242f3dd174f5f2edf68
sahilverma2k <61312202+sahilverma2k@users.noreply.github.com>
2020-10-14 15:33:45 +0530

    subtotal component

--- frontend/src/Subtotal.js
+++ frontend/src/Subtotal.js
@@ -9,19 +9,20 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items) : <strong>0</strong>
+              Subtotal (0 items): <strong>0</strong>
             </p>
             <small className="subtotal_gift">
               <input type="checkbox" /> This order contains a gift
             </small>
           </>
         )}
-        decimalScale={2}
+        decimalscale={2}
         value={0}
-        displayTypes={"text"}
+        displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
       />
+      <button>Proceed to checkout</button>
     </div>
   );
 }
