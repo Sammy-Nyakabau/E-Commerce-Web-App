03ff7bfe50c56ab0038072d5f0e7f6cdfd33d795
sahilverma2k <61312202+sahilverma2k@users.noreply.github.com>
2020-10-17 23:10:55 +0530

    order page

---
+++ frontend/src/components/Order_page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Order_page.css";
+
+
+
+function Order_page() {
+  return (
+    <div className="order">
+      <div className="order_banner">
+        <img src="https://i.pinimg.com/originals/0f/0d/58/0f0d5833c0d8aa0d164536e371958c94.jpg" />
+        <div className="order_banner_heading">
+          <h1>YOUR ORDERS</h1>
+        </div>
+        <div className="order_product">
+         
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Order_page;
