66c4a9158e7d31d668fd676b07981e4b2bfc627f
Sammy Nyakabau <nyakabausammy8@gmail.com>
2020-10-20 13:51:44 +0530

    Add Review services 💯

--- frontend/src/components/Order_page.js
+++ frontend/src/components/Order_page.js
@@ -5,50 +5,49 @@ import { getMyOrders } from "../services/ordersService";
 import Order_component from "./Order_component";
 
 function Order_page() {
-  const [{ user }] = useStateValue();
-  const [products, setproducts] = useState([]) //this stores all ordered products
-  let orderedProducts = []
+  const [{ user }, dispatch] = useStateValue();
+  const [products, setproducts] = useState([]); //this stores all ordered products
+  let orderedProducts = [];
 
   useEffect(() => {
     async function getOrders() {
       const { data: userOrders } = await getMyOrders(user._id);
-      userOrders.forEach(order => {
-        order.orderItems.forEach(prod => {
-          orderedProducts.push(prod)
+      userOrders.forEach((order) => {
+        order.orderItems.forEach((prod) => {
+          orderedProducts.push(prod);
         });
       });
-      setproducts(orderedProducts)
-      }
-      getOrders();
+      setproducts(orderedProducts);
+      dispatch({
+        type: "ADD_ORDERS",
+        item: products,
+      });
     }
-  );
+    getOrders();
+  });
 
   //products array stores all the ordered products by the logged in user
-
 
   return (
     <div className="order">
       <div className="order_banner">
-        <img
-          alt=""
-          src="https://wallpapercave.com/wp/rE73Sms.jpg"
-        />
+        <img alt="" src="https://wallpapercave.com/wp/rE73Sms.jpg" />
         <div className="order_banner_heading">
           <h1>YOUR ORDERS</h1>
         </div>
         <div className="order_product">
-        <div class="scrollboxorder">
-          <span />
-          <span />
-          <span />
-        </div>
-        <div className="order_home">
-          <div className="row">
-            {products.map((product) => (
-              <Order_component key={product.id} product={product} />
-            ))}
+          <div class="scrollboxorder">
+            <span />
+            <span />
+            <span />
           </div>
-        </div>
+          <div className="order_home">
+            <div className="row">
+              {products.map((product) => (
+                <Order_component key={product.id} product={product} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
