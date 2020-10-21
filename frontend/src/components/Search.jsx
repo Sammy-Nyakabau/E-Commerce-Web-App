8b17dd56377dad8943c2c8bf0e6527a24cf8b7b0
sahilverma2k <61312202+sahilverma2k@users.noreply.github.com>
2020-10-21 22:52:14 +0530

    search css

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import React, { Component } from "react";
-import "./Shop.css";
+import "./Search.css";
 import ProductList from "./ProductList";
 import { getProducts } from "../services/productService";
 import "./grid.css";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
 
 class Shop extends Component {
   state = {
@@ -26,17 +27,22 @@ class Shop extends Component {
 
     return (
       <div className="shop">
-        <div id="search__banner">
-          <img src="https://i.ibb.co/1vL1YBf/Sukhoi-Su-30-military-aircraft-man-black-background-1920x1080.jpg" />
+        <div id="shop_banner">
         </div>
 
         <div className="shop_banner_heading">
           <h1>Search</h1>
         </div>
-        
+        <form>
+          <label for="fname">Search</label>
+          <input className="inputbox" type="text" id="fname" name="fname" />
+        </form>
+
         {/* <div className="shop_bar">
         </div> */}
+        <div className="search_content">
         <ProductList products={this.state.products} />
+        </div>
       </div>
     );
   }
